9780a1204486b4ba017ec296e3b88e9ce3cd9000
Shaykhraziev Bulat <mrcookie209@gmail.com>
2025-03-01 00:33:27 +0300

    fixed 500 error

--- src/main/kotlin/ru/kotleteri/database/tables/CompanyTable.kt
+++ src/main/kotlin/ru/kotleteri/database/tables/CompanyTable.kt
@@ -1,13 +1,11 @@
 package ru.kotleteri.database.tables
 
 import org.jetbrains.exposed.dao.id.IdTable
-import ru.kotleteri.database.tables.ClientTable.entityId
-import ru.kotleteri.database.tables.ClientTable.uniqueIndex
-import java.util.UUID
+import java.util.*
 
 object CompanyTable: IdTable<UUID> ("company") {
     override val id = ClientTable.uuid("id").uniqueIndex().entityId()
     val name = varchar("name", 50)
     val email = ClientTable.varchar("email", 128)
-    val password = UsersTable.varchar("password", 60).uniqueIndex()
+    val password = varchar("password", 60)
 }
