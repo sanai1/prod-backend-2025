7c575104acae4e561f36deb82af3aa823238b0f2
Kolyakot33 <kolyakot33@gmail.com>
2025-03-01 01:59:13 +0300

    Implement methods

--- src/main/kotlin/ru/kotleteri/database/crud/OfferCRUD.kt
+++ src/main/kotlin/ru/kotleteri/database/crud/OfferCRUD.kt
@@ -1,8 +1,11 @@
 package ru.kotleteri.database.crud
 
-import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import ru.kotleteri.data.models.base.OfferModel
+import ru.kotleteri.database.suspendTransaction
 import ru.kotleteri.database.tables.OfferTable
+import java.util.*
 
 object OfferCRUD {
     fun resultRowToOffer(resultRow: ResultRow): OfferModel =
@@ -15,4 +18,49 @@ object OfferCRUD {
             startDate = resultRow[OfferTable.startDate],
             endDate = resultRow[OfferTable.endDate]
         )
+
+    suspend fun create(offer: OfferModel) =
+        suspendTransaction {
+            OfferTable.insert {
+                it[id] = offer.id
+                it[companyId] = offer.companyId
+                it[title] = offer.title
+                it[description] = offer.description
+                it[discount] = offer.discount
+                it[startDate] = offer.startDate
+                it[endDate] = offer.endDate
+            }
+        }
+
+    suspend fun read(id: UUID) =
+        suspendTransaction {
+            OfferTable.selectAll()
+                .where { OfferTable.id eq id }
+                .singleOrNull()
+                ?.let { resultRowToOffer(it) }
+        }
+
+    suspend fun readByCompanyId(companyId: UUID) =
+        suspendTransaction {
+            OfferTable.selectAll()
+                .where { OfferTable.companyId eq companyId }
+                .map { resultRowToOffer(it) }
+        }
+
+    suspend fun update(offer: OfferModel) =
+        suspendTransaction {
+            OfferTable.update({ OfferTable.id eq offer.id }) {
+                it[companyId] = offer.companyId
+                it[title] = offer.title
+                it[description] = offer.description
+                it[discount] = offer.discount
+                it[startDate] = offer.startDate
+                it[endDate] = offer.endDate
+            }
+        }
+
+    suspend fun delete(id: UUID) =
+        suspendTransaction {
+            OfferTable.deleteWhere { OfferTable.id eq id }
+        }
 }
