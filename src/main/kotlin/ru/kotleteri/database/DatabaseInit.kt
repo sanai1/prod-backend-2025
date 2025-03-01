b0daa2e1ff0df6d51310769f7cd52921c54bfa6a
Kolyakot33 <kolyakot33@gmail.com>
2025-03-01 11:45:38 +0300

    Fix add offertable

--- src/main/kotlin/ru/kotleteri/database/DatabaseInit.kt
+++ src/main/kotlin/ru/kotleteri/database/DatabaseInit.kt
@@ -5,12 +5,14 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
 import ru.kotleteri.database.tables.ClientTable
 import ru.kotleteri.database.tables.CompanyTable
+import ru.kotleteri.database.tables.OfferTable
 
 object DatabaseInit {
     private val tables: List<Table> =
         listOf(
             ClientTable,
-            CompanyTable
+            CompanyTable,
+            OfferTable
         )
 
     fun initialize() {
