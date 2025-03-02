ef21dbd6b603480778bcb241fafbcb05cf1a7ce0
Shaykhraziev Bulat <mrcookie209@gmail.com>
2025-03-02 11:38:35 +0300

    changed stats fields name

--- src/main/kotlin/ru/kotleteri/data/models/inout/statistics/StatisticsDateResponseModel.kt
+++ src/main/kotlin/ru/kotleteri/data/models/inout/statistics/StatisticsDateResponseModel.kt
@@ -6,7 +6,7 @@ import java.time.LocalDate
 @Serializable
 data class StatisticsDateResponseModel(
     val date: String,
-    val operations_count: Int,
-    val male_operations_count: Int,
-    val female_operations_count: Int
+    val operations_amount: Int,
+    val male_operations_amount: Int,
+    val female_operations_amount: Int
 )
