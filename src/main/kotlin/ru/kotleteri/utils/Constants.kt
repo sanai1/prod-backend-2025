685e1b462b8a37ed96ef09f8bc531a6d1ae25081
Shaykhraziev Bulat <mrcookie209@gmail.com>
2025-03-02 03:51:12 +0300

    changed jwt lifetime

--- src/main/kotlin/ru/kotleteri/utils/Constants.kt
+++ src/main/kotlin/ru/kotleteri/utils/Constants.kt
@@ -3,4 +3,4 @@ package ru.kotleteri.utils
 const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$"
 const val PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,}\$"
 
-const val JWT_LIFETIME = 1000L * 60L * 1L
+const val JWT_LIFETIME = 1000L * 60L * 60L * 12L
